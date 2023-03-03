48792e28cf5a46d6abb94b838d15521ea868b9c5
YahirMCM <126836959+YahirMCM@users.noreply.github.com>
2023-03-02 19:39:15 -0600

    Confirmación-Código-Evidencia1

--- Evidencia_01_EDP.py
+++ Evidencia_01_EDP.py
@@ -4,3 +4,6 @@ print("martincito")
 #Esta es una prueba para los commit
 
 print("Prueba")
+
+#Ahora bien, aquí comenzamos la evidencia de aprendizaje
+# A partir de este comentario comenzar a codificar
