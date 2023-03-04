cc840ae6fc02a02351c7a9e97078c8c41639c0b6
YahirMCM <126836959+YahirMCM@users.noreply.github.com>
2023-03-03 22:32:01 -0600

    Correción_02 (Y avance en consulta)

--- Evidencia_01_EDP.py
+++ Evidencia_01_EDP.py
@@ -9,17 +9,19 @@ while True:
   if op_main == 1:
     # Registro de datos
     while True:
-      identificador = max(datos.key,default=0)+1
-      titulo.upper() = input("Dame el nombre del libro: \n")
-      autor.upper() = input(f"Dame el autor de {titulo}: \n")
-      genero.upper() = input(f"Cual es el genero de {titulo}: \n")
-      año_publicacion.upper() = input(f"En que año se publico {titulo}: \n")
-      ISBN.upper() = input(f"Cual es el ISBN de {titulo}: \n")
-      fecha_adquisicion.upper() = input(f"Cuando se adquirio {titulo}: \n")
+      identificador = max(datos, default=0)+1
+      titulo = input("Dame el nombre del libro: \n").upper()
+      autor = input(f"Dame el autor de {titulo}: \n").upper()
+      genero = input(f"Cual es el genero de {titulo}: \n").upper()
+      año_publicacion = input(f"En que año se publico {titulo}: \n").upper()
+      ISBN = input(f"Cual es el ISBN de {titulo}: \n").upper()
+      fecha_adquisicion = input(f"Cuando se adquirio {titulo}: \n").upper()
       print("Datos cargados!")
-      op_registro = int(input("Deseas agregar mas?(clickea enter) \n"))
-      if op_registro = "":
+      op_registro = input("Deseas agregar mas?(clickea enter) \n")
+      if op_registro == "":
         break
+      else:
+        datos[identificador] = [titulo,autor,genero,año_publicacion,ISBN,fecha_adquisicion]
   elif op_main == 2:
     # Consultas y Reportes
     while True:
@@ -39,14 +41,15 @@ while True:
           print("[3]- Regresar al menu anterior")
           op_busqueda = int(input())
           if op_busqueda == 1:
-            
+            for key in datos:
+              print(datos[identificador])
           elif op_busqueda == 2:
-
+            print('ISBN')
           elif op_busqueda == 3:
             break
       elif op_consulta == 2:
         # Reportes tabulados
-      
+        print('Reportes')
       elif op_consulta == 3:
         print('Volviendo al menú principal . . .')
         break
