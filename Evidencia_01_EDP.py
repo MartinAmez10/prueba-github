11d675b1b7383ef462ef3e50aca318baf91597ec
Brandon115714 <126829936+Brandon115714@users.noreply.github.com>
2023-03-03 20:45:25 -0600

    Update Evidencia_01_EDP.py

--- Evidencia_01_EDP.py
+++ Evidencia_01_EDP.py
@@ -1,4 +1,4 @@
-
+datos = {}
 while True:
   print("Hola! selecciona una opcion que quieras realizar (escribe el numero):")
   print("[1]- Registrar nuevo ejemplar")
@@ -7,6 +7,10 @@ while True:
   op = int(input())
   if op == 1:
     # agrega datos
+    identificador = int(input("Dame la clave: \n"))
+    if identificador in datos:
+      print("esa clave ya existe")
+      break
   if op == 2:
     # consulta los datos guardados
   if op == 3:
