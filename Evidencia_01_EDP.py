5da668b0ac77f6744a86ef0fc829b1179c179dbc
Brandon115714 <126829936+Brandon115714@users.noreply.github.com>
2023-03-03 21:20:50 -0600

    modificacion opcion 1

--- Evidencia_01_EDP.py
+++ Evidencia_01_EDP.py
@@ -7,14 +7,18 @@ while True:
   op = int(input())
   
   if op == 1:
-    # Agrega datos
-    identificador = max(datos.key,default=0)+1
-    titulo.upper() = input("Dame el nombre del libro: \n")
-    autor.upper() = input(f"Dame el autor de {titulo}: \n")
-    genero.upper() = input(f"Cual es el genero de {titulo}: \n")
-    año_publicacion.upper() = input(f"En que año se publico {titulo}: \n")
-    ISBN.upper() = input(f"Cual es el ISBN de {titulo}: \n")
-    fecha_adquisicion.upper() = input(f"Cuando se adquirio {titulo}: \n")
+    while True:
+      identificador = max(datos.key,default=0)+1
+      titulo.upper() = input("Dame el nombre del libro: \n")
+      autor.upper() = input(f"Dame el autor de {titulo}: \n")
+      genero.upper() = input(f"Cual es el genero de {titulo}: \n")
+      año_publicacion.upper() = input(f"En que año se publico {titulo}: \n")
+      ISBN.upper() = input(f"Cual es el ISBN de {titulo}: \n")
+      fecha_adquisicion.upper() = input(f"Cuando se adquirio {titulo}: \n")
+      print("Datos cargados!")
+      op = int(input("Deseas agregar mas?(clickea enter) \n"))
+      if op = "":
+        break
   elif op == 2:
     # Consulta los datos guardados
   elif op == 3:
