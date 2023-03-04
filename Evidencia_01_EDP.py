72faa89c41566fa2a0aa400f5fb34942afab395d
Brandon115714 <126829936+Brandon115714@users.noreply.github.com>
2023-03-03 21:37:07 -0600

    agregacion en menus

--- Evidencia_01_EDP.py
+++ Evidencia_01_EDP.py
@@ -4,9 +4,9 @@ while True:
   print("[1]- Registrar nuevo ejemplar")
   print("[2]- Consultas y Reportes")
   print("[3]- Salir")
-  op = int(input())
+  op_main = int(input())
   
-  if op == 1:
+  if op_main == 1:
     while True:
       identificador = max(datos.key,default=0)+1
       titulo.upper() = input("Dame el nombre del libro: \n")
@@ -16,17 +16,30 @@ while True:
       ISBN.upper() = input(f"Cual es el ISBN de {titulo}: \n")
       fecha_adquisicion.upper() = input(f"Cuando se adquirio {titulo}: \n")
       print("Datos cargados!")
-      op = int(input("Deseas agregar mas?(clickea enter) \n"))
-      if op = "":
+      op_registro = int(input("Deseas agregar mas?(clickea enter) \n"))
+      if op_registro = "":
         break
-  elif op == 2:
+  elif op_main == 2:
     while True:
       print("Selecciona una opcion que quieras realizar (escribe el numero):")
       print("[1]- Consulta de titulo")
       print("[2]- Reportes")
       print("[3]- Regresar al menu principal")
-      op = int(input())
-      
-  elif op == 3:
+      op_consulta = int(input())
+      if op_consulta == 1:
+        while True:
+          print("De que forma deseas buscar el libro?(escribe el numero):")
+          print("[1]- Por titulo")
+          print("[2]- Por ISBN")
+          print("[3]- Regresar al menu anterior")
+          op_busqueda = int(input())
+          if op_busqueda == 1:
+
+          elif op_busqueda == 2:
+
+          elif op_busqueda == 3:
+            break
+
+  elif op_main == 3:
     # Sale del programa
     break
