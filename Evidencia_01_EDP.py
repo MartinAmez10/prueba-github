1f4196c467be6d27a2ee2ecf9563100e39b3e8d6
Brandon115714 <126829936+Brandon115714@users.noreply.github.com>
2023-03-03 20:21:01 -0600

    Update Evidencia_01_EDP.py

--- Evidencia_01_EDP.py
+++ Evidencia_01_EDP.py
@@ -1,9 +1,13 @@
-print('Hello World!')
-print("martincito")
 
-#Esta es una prueba para los commit
-
-print("Prueba")
-
-#Ahora bien, aquí comenzamos la evidencia de aprendizaje
-# A partir de este comentario comenzar a codificar
+while True:
+  print("Hola! selecciona una opcion que quieras realizar (escribe el numero):")
+  print("[1]- Registrar nuevo ejemplar")
+  print("[2]- Consultas y Reportes")
+  print("[3]- Salir")
+  op = int(input())
+  if op == 1:
+    # agrega datos
+  if op == 2:
+    # consulta los datos guardados
+  if op == 3:
+    break
