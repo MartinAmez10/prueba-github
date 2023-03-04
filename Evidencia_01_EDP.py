ae3330dd24671352f49a344a3e07633c27048bda
Brandon115714 <126829936+Brandon115714@users.noreply.github.com>
2023-03-03 20:54:39 -0600

    Update Evidencia_01_EDP.py

--- Evidencia_01_EDP.py
+++ Evidencia_01_EDP.py
@@ -7,10 +7,13 @@ while True:
   op = int(input())
   if op == 1:
     # agrega datos
-    identificador = int(input("Dame la clave: \n"))
-    if identificador in datos:
-      print("esa clave ya existe")
-      break
+    identificador = max(datos.key,default=0)+1
+    titulo.upper = input("Dame el nombre del libro: \n")
+    autor.upper = input(f"Dame el autor de {titulo}: \n")
+    genero.upper = input(f"Cual es el genero de {titulo}: \n")
+    año_publicacion.upper = input(f"En que año se publico {titulo}: \n")
+    ISBN.upper = input(f"Cual es el ISBN de {titulo}: \n")
+    fecha_adquisicion.upper = input(f"Cuando se adquirio {titulo}: \n")
   if op == 2:
     # consulta los datos guardados
   if op == 3:
